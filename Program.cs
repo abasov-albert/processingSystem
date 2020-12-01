0478ddfbb1783fd5dc80e74331d2ac4e478eec70
Albert Abasov <abasovalik2001@gmail.com>
2020-12-01 18:43:50 +0200

    add program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Lab2.Classes;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process(DateTime.UtcNow.Millisecond, 1, 11001);
+            Console.WriteLine(process.ToString());
+            Console.WriteLine();
+            var processCharacteristics = process.toArray();
+            foreach (var i in processCharacteristics)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
